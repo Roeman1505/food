5b17a3c2ac0006e4614bd1af0ed4c2c9b55fc2ee
Roeman1505 <robinsonemmanuel1505@gmail.com>
2022-08-30 01:50:18 +0300

    On tap Function for bottom navigation

--- lib/home.dart
+++ lib/home.dart
@@ -10,26 +10,54 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   // ignore: todo
   //TODO: Add State Variables and Function
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget>[
+    // ignore: todo
+    //TODO:Replace with Card1
+    Container(color: Colors.red),
+    // ignore: todo
+    //TODO:Replace with Card2
+    Container(color: Colors.green),
+    // ignore: todo
+    //TODO:Replace with Card3
+    Container(color: Colors.blue),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+       _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text(
-      'Food',
-      style: Theme.of(context).textTheme.headline6,
-    )),
-    // ignore: todo
-    //TODO:Show Selected Tab
-    body:Center(
-      child:Text(
-        'Let\'s get Cooking',
-        style:Theme.of(context).textTheme.headline1
-      
-      ) ,
-      ),
+      appBar: AppBar(
+          title: Text(
+        'Food',
+        style: Theme.of(context).textTheme.headline6,
+      )),
+      // ignore: todo
+      //TODO:Show Selected Tab
+      body: pages[_selectedIndex],
       // ignore: todo
       //TODO: Add Bottom Navigation bar
-    
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor:
+              Theme.of(context).textSelectionTheme.selectionColor,
+
+          // ignore: todo
+          //TODO:Set selected tab Bar\
+          currentIndex:_selectedIndex,
+          onTap:_onItemTapped,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card1'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card2'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card3'),
+          ]),
     );
   }
 }
